661f888c166520b7304a507cd661a309f500b46e
user <user@machine>
2022-03-30 11:17:16 -0400

    Minor fixes;

--- logger.h
+++ logger.h
@@ -3,11 +3,9 @@
  * 
  * See COPYING.txt in the project root for license information.
  */
-
 #include <string>
 #include <iostream>
 #include <fstream>
-#include "nlohmann/json.hpp"
 
 using string = std::string;
 
@@ -18,37 +16,36 @@ class Logger
 public:
     virtual void write(std::ofstream &of) = 0;
     virtual void write(std::ostream &os) = 0;
-    virtual int16_t get_msg_code() const = 0;
+    virtual int16_t get_message_code() const = 0;
     
-    Logger() { };
-    virtual ~Logger() { };
+    Logger() {};
+    virtual ~Logger() {};
 };
 
 class Json_logger: public Logger
 {
 private:
-    int16_t msg_code;
-    string  msg;
+    int16_t message_code;
+    string  message;
     string  context;
 public:
-    void write(std::ofstream &of)   { of << msg << '|' << context << '\n'; };
-    void write(std::ostream &os)    { os << msg << '|' << context << '\n'; };
-    int16_t get_msg_code() const    { return msg_code; }
+    void write(std::ofstream &of)   { of << message << '|' << context << '\n'; }
+    void write(std::ostream &os)    { os << message << '|' << context << '\n'; }
+    int16_t get_message_code() const { return message_code; }
 
     Json_logger(int16_t code, string message);
     Json_logger(int16_t code, string message, string context);
-    virtual ~Json_logger() { };
+    virtual ~Json_logger() {};
 };
 
-Json_logger::Json_logger(int16_t code, string message):
-    msg_code(code),
-    msg(std::move(message))
-{ };
+Json_logger::Json_logger(int16_t message_code, string message):
+    message_code(message_code),
+    message(std::move(message))
+{};
 
-Json_logger::Json_logger(int16_t code, string message, string context):
-    msg_code(code),
-    msg(std::move(message)),        
+Json_logger::Json_logger(int16_t message_code, string message, string context):
+    message_code(message_code),
+    message(std::move(message)),        
     context(std::move(context))
-{ };
-
+{};
 }
